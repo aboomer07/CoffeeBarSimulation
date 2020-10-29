6c4f8175d39283902bcf40c828770c4c9f4600e5
Andy Boomer <aboomer07@gmail.com>
2020-10-28 18:03:06 -0700

    Updated This File For the Upated Probability Calculation

--- Code/Customer_Probabilities.py
+++ Code/Customer_Probabilities.py
@@ -1,59 +1,55 @@
 ################################################################################
+# Part 1 of Python Final Project: Simulation of Coffee Drinkers
+# Group Partners: Andy Boomer and Jacob Pichelmann
+# Part 1 involves exploring sample coffee dataset, creating probabilities
+################################################################################
+
+################################################################################
 # Importing Libraries and Datafile
+import numpy as np
 import pandas as pd
+import re
 import os
+import sys
 
-#os.chdir('/Users/jacobpichelmann/PycharmProjects/examtse2020-21_andy/Code')
-
-data_path = os.path.abspath('..') + \
+DataPath = os.path.abspath('..') + \
 	"/Data/Coffeebar_2016-2020.csv"
 
-
-df = pd.read_csv(data_path, sep = ";")
+df = pd.read_csv(DataPath, sep = ";")
 ################################################################################
 
 ################################################################################
 # Perform initial data manipulation
 df['TIME'] = pd.to_datetime(df['TIME'], format = "%Y-%m-%d %H:%M:%S")
-df['HOUR'] = df['TIME'].dt.hour
-df['MINUTE'] = df['TIME'].dt.minute
-df['YEAR'] = df['TIME'].dt.year
+df['HOUR'] = df['TIME'].dt.hour #Get Hour Integer
+df['MINUTE'] = df['TIME'].dt.minute #Get Minute Integer
+df['YEAR'] = df['TIME'].dt.year #Get Year Integer
 
-df['FOOD'] = df['FOOD'].fillna("nothing")
+df['FOOD'] = df['FOOD'].fillna("nothing") #A null value means no food ordered
 ################################################################################
 
 ################################################################################
 # What food and drink are sold?
-drink_list = df['DRINKS'].unique()
-food_list = df['FOOD'].unique()
+DrinkList = df['DRINKS'].unique() #Get the unique list of possible drinks
+FoodList = df['FOOD'].unique() #Get unique list of possible food items
 
+# How many unique customers did the bar have?
+CustomerList = df['CUSTOMER'].unique() #Unique set of customers ID's
+################################################################################
 
 ################################################################################
-# Calculating Probabilities For Food and Drinks per time interval
-food_probs = df.groupby(['HOUR', 'MINUTE', 'FOOD'],
-	as_index = False).count()
-food_probs = food_probs[['HOUR', 'MINUTE', 'FOOD', 'TIME']]
-food_probs = food_probs.rename({"TIME" : "COUNT"}, axis = 1)
-food_probs = food_probs.set_index(['HOUR', 'MINUTE', 'FOOD']).unstack('FOOD')
-# food_probs = food_probs.pivot(index=['HOUR', 'MINUTE'], columns='FOOD', values='COUNT') this threw an error
-food_probs = food_probs.fillna(0)
-food_probs['total'] = food_probs.sum(axis=1)
-#food_probs.columns.name = None
-food_probs.reset_index(inplace=True)
-food_probs.columns = ['HOUR', 'MINUTE', 'cookie', 'muffin', 'nothing', 'pie', 'sandwich', 'total']
-food_probs[food_list] = food_probs[food_list].div(food_probs['total'], axis=0)
-food_probs['max_prob'] = food_probs[food_list].idxmax(axis=1)
+# Calculating Probabilities For Food and Drinks per 5-Minute Interval
+#Use Value Counts to Calculate
+FoodProbs = df.groupby(['HOUR', 'MINUTE'])['FOOD'].\
+	value_counts(normalize = True).\
+	unstack(fill_value = 0).\
+	reset_index().\
+	rename_axis(None, axis = 1)
 
 
-drink_probs = df.groupby(['HOUR', 'MINUTE', 'DRINKS'],
-	as_index = False).count()
-drink_probs = drink_probs[['HOUR', 'MINUTE', 'DRINKS', 'TIME']]
-drink_probs = drink_probs.rename({"TIME" : "COUNT"}, axis = 1)
-drink_probs = drink_probs.set_index(['HOUR', 'MINUTE', 'DRINKS']).unstack('DRINKS')
-drink_probs = drink_probs.fillna(0)
-drink_probs['total'] = drink_probs.sum(axis=1)
-drink_probs.reset_index(inplace = True)
-drink_probs.columns = ['HOUR', 'MINUTE', 'coffee', 'frappucino', 'milkshake', 'soda', 'tea', 'water', 'total']
-drink_probs[drink_list] = drink_probs[drink_list].div(drink_probs['total'], axis=0)
-drink_probs['max_prob'] = drink_probs[drink_list].idxmax(axis=1)
+DrinkProbs = df.groupby(['HOUR', 'MINUTE'])['DRINKS'].\
+	value_counts(normalize = True).\
+	unstack(fill_value = 0).\
+	reset_index().\
+	rename_axis(None, axis = 1)
 ################################################################################
