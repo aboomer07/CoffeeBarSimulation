29083f203ca0dbf7c33a7ac49675704a4aebfa9a
Andy Boomer <aboomer07@gmail.com>
2020-10-31 19:28:23 -0700

    Added datetime instead of hour/minute, commented the code, and added a conditional to check if customer has necessary budget

--- Code/Customers.py
+++ Code/Customers.py
@@ -10,90 +10,122 @@
 
 
 # import from exploratory script
-from Code.Customer_Probabilities import food_probs, drink_probs, food_list, drink_list
+from Customer_Probabilities import food_probs, drink_probs, food_list, drink_list
 # Import libraries
 import uuid
 import names  # just for fun
 import numpy as np
+import datetime  # For getting hour and minute from datetime objects
 
 # Each customer has a customer_id and a certain budget. Based on the shape example in the slides the budget
 # is not introduced at the super class level.
 
 # set up menu of prices - maybe in other script?
-menu = {'sandwich': 2, 'cookie': 2, 'pie': 3, 'muffin': 3,
-        'milkshake': 5, 'frappucino': 4, 'water': 2, 'coffee': 3, 'soda': 3, 'tea': 3,
-        'nothing': 0}
+menu = {'sandwich': 2, 'cookie': 2, 'pie': 3, 'muffin': 3, 'milkshake': 5,
+        'frappucino': 4, 'water': 2, 'coffee': 3, 'soda': 3, 'tea': 3, 'nothing': 0}
+
 
 class Customer(object):
     def __init__(self):
-        self.customer_id = uuid.uuid4()  # less than a one in a trillion chance of repeating itself
+        # less than a one in a trillion chance of repeating itself
+        self.customer_id = uuid.uuid4()
         self.budget = 100
         self.name = names.get_first_name()  # just for fun
+        # initialize variables for the customer
         self.food_choice = None
         self.drink_choice = None
         self.amount_spent = None
         self.time = None
 
-    def make_choice(self, hour, minute):
-        food_prob = \
-            food_probs[(food_probs['hour'] == hour) & (food_probs['minute'] == minute)][food_list].values.tolist()[0]
+    def make_choice(self, time):
+        hour = time.hour  # Get the hour to be used in probability
+        minute = time.minute  # Get the minute to be used in probability
+
+        # Use the food list and probabilities to make a food choice
+        food_prob = food_probs[(food_probs['hour'] == hour) & (
+            food_probs['minute'] == minute)][food_list].values.tolist()[0]
         food_choice = np.random.choice(food_list, 1, p=food_prob)[0]
-        drink_prob = \
-            drink_probs[(drink_probs['hour'] == hour) & (drink_probs['minute'] == minute)][drink_list].values.tolist()[
-                0]
+
+        # Use the drink list and probabilities to make a drink choice
+        drink_prob = drink_probs[(drink_probs['hour'] == hour) & (
+            drink_probs['minute'] == minute)][drink_list].values.tolist()[0]
         drink_choice = np.random.choice(drink_list, 1, p=drink_prob)[0]
-        self.food_choice = food_choice
-        self.drink_choice = drink_choice
-        self.time = [hour, minute]
+
+        # We need to see if the customer has the budget for their choices
+        if self.budget - (menu[food_choice] + menu[drink_choice]) >= 0:
+            # Overwrite the initialized variables
+            self.food_choice = food_choice
+            self.drink_choice = drink_choice
+        else:
+            # If customer can't afford, they should choose nothing
+            # This will return an error when they try to make a payment
+            # This error can be handled in the simulation with a try statement
+            self.food_choice = None
+            self.drink_choice = None
+        self.time = time
 
     def show_budget(self):
+        # Display the customer's current budget
         print(self.name + '\'s budget is ' + str(self.budget))
 
     def make_payment(self):
+        # Make the payment based on the price in the menu and the choices
         self.amount_spent = menu[self.food_choice] + menu[self.drink_choice]
+        # Reduce the reminining budget accordingly
         self.budget = self.budget - self.amount_spent
 
     def tell_purchase(self):
+        # Display what the customer purchased and at what time
         print(self.name + ' bought ' + self.drink_choice + ' and ' + self.food_choice + ' for a total price of ' +
-              str(self.amount_spent) + ' at ' + str(self.time[0]) + ':' + str(self.time[1]))
+              str(self.amount_spent) + ' at ' + str(self.time))
 
 
-class ReturningCustomer(Customer):
+class ReturningCustomer(Customer):  # Define a returning customer
     def __init__(self):
         super(ReturningCustomer, self).__init__()
-        self.budget += 150
-        self.history = {}
-        self.visit = 0
+        self.budget += 150  # they have a higher budget
+        self.history = {}  # They also can remember their purchase history
+        self.visit = 0  # Keep track of their current visit number
 
     def make_payment(self):
+        # Their make payment function is slightly different with the history
         self.amount_spent = menu[self.food_choice] + menu[self.drink_choice]
         self.budget = self.budget - self.amount_spent
-        self.visit += 1
-        history = {'Visit ' + str(self.visit): [self.drink_choice, self.food_choice, self.time]}
+        self.visit += 1  # Increase the visit variable by 1
+        history = {  # Create a new entry in the dict for the current visit
+            'Visit ' + str(self.visit): [self.drink_choice, self.food_choice, str(self.time)]}
         self.history.update(history)
 
     def tell_purchase_history(self):
-        print(self.history)
+        print(self.history)  # Show the current history dictionary
 
 
 class Hipster(ReturningCustomer):
+    # Hipsters are a type of returning customer, we chose to make it a subclass
+    # In case we want the hipster to have some different behavior later
     def __init__(self):
         super(Hipster, self).__init__()
-        self.budget += 250
+        self.budget += 250  # Hipsters have an even larger budget
 
 
 class TripAdvisorCustomer(Customer):
+    # Trip advisor is a subclass of customer that adds a random tip to purchase
     def make_payment(self):
-        self.amount_spent = menu[self.food_choice] + menu[self.drink_choice] + np.random.randint(1, 10)
+        self.amount_spent = menu[self.food_choice] + \
+            menu[self.drink_choice] + np.random.randint(1, 10)
         self.budget = self.budget - self.amount_spent
 
 
-Cust3 = Hipster()
+# Now test these classes for a few different actions the customers can do
+time1 = datetime.datetime(2020, 6, 25, 8, 0, 0)
+time2 = datetime.datetime(2020, 10, 10, 14, 32, 0)
 
-Cust3.make_choice(12, 32)
-Cust3.make_payment()
-Cust3.tell_purchase_history()
+Cust3 = Hipster()  # Create hipster returning customer
 
-Cust3.make_choice(14, 28)
-Cust3.make_payment()
-Cust3.tell_purchase_history()
+Cust3.make_choice(time1)  # Have them make their first choice
+Cust3.make_payment()  # Make the payment based on the choice
+Cust3.tell_purchase_history()  # Return their history
+
+Cust3.make_choice(time2)  # They visit again at another time
+Cust3.make_payment()  # They make the payment for current visit
+Cust3.tell_purchase_history()  # They have a new updated history
