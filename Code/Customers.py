0618a5684396dff7c7b25edbb4c612a9b5255943
Andy Boomer <aboomer07@gmail.com>
2020-11-07 23:46:48 -0800

    Adjusting for Flexibility

--- Code/Customers.py
+++ Code/Customers.py
@@ -14,17 +14,13 @@ import names  # just for fun, can be installed with sudo pip
 import numpy as np
 import datetime  # For getting hour and minute from datetime objects
 
-# set up menu of prices - maybe in other script?
-menu = {'sandwich': 2, 'cookie': 2, 'pie': 3, 'muffin': 3, 'milkshake': 5,
-        'frappucino': 4, 'water': 2, 'coffee': 3, 'soda': 3, 'tea': 3, 'nothing': 0}
-
 
 class Customer(object):
-    def __init__(self):
+    def __init__(self, params):
         # less than a one in a trillion chance of repeating itself
         self.customer_id = uuid.uuid4()
         self.customer_type = 'one_time'
-        self.budget = 100
+        self.budget = params[self.customer_type]['budget']
         self.name = names.get_first_name()  # just for fun
         # initialize variables for the customer
         self.food_choice = None
@@ -32,32 +28,22 @@ class Customer(object):
         self.amount_spent = None
         self.time = None
 
-    def make_choice(self, time, food_choice, drink_choice):
-        # hour = time.hour  # Get the hour to be used in probability
-        # minute = time.minute  # Get the minute to be used in probability
-
-        # Use the food list and probabilities to make a food choice
-        # food_prob = food_probs[(food_probs['hour'] == hour) & (
-        #     food_probs['minute'] == minute)][food_list].values.tolist()[0]
-        # food_choice = np.random.choice(
-        #     list(food_prob.keys()), 1, p=list(food_prob.values()))[0]
-
-        # Use the drink list and probabilities to make a drink choice
-        # drink_prob = drink_probs[(drink_probs['hour'] == hour) & (
-        #     drink_probs['minute'] == minute)][drink_list].values.tolist()[0]
-        # drink_choice = np.random.choice(
-        #     list(drink_prob.keys()), 1, p=list(drink_prob.values()))[0]
-
+    def make_choice(self, time, foods, drinks):
         # Overwrite the initialized variables
-        self.food_choice = food_choice
-        self.drink_choice = drink_choice
+
         self.time = time
+
+        self.food_choice = np.random.choice(
+            list(foods.keys()), 1, p=list(foods.values()))[0]
+
+        self.drink_choice = np.random.choice(
+            list(drinks.keys()), 1, p=list(drinks.values()))[0]
 
     def show_budget(self):
         # Display the customer's current budget
         print(self.name + '\'s budget is ' + str(self.budget))
 
-    def make_payment(self):
+    def make_payment(self, menu):
         # Make the payment based on the price in the menu and the choices
         self.amount_spent = menu[self.food_choice] + menu[self.drink_choice]
         # Reduce the reminining budget accordingly
@@ -70,14 +56,15 @@ class Customer(object):
 
 
 class ReturningCustomer(Customer):  # Define a returning customer
-    def __init__(self):
-        super(ReturningCustomer, self).__init__()
+    def __init__(self, params):
+        super(ReturningCustomer, self).__init__(params)
         self.customer_type = 'returning'
-        self.budget += 150  # they have a higher budget
+        # they have a higher budget
+        self.budget = params[self.customer_type]['budget']
         self.history = {}  # They also can remember their purchase history
         self.visit = 0  # Keep track of their current visit number
 
-    def make_payment(self):
+    def make_payment(self, menu):
         # Their make payment function is slightly different with the history
         self.amount_spent = menu[self.food_choice] + menu[self.drink_choice]
         self.budget = self.budget - self.amount_spent
@@ -93,22 +80,68 @@ class ReturningCustomer(Customer):  # Define a returning customer
 class Hipster(ReturningCustomer):
     # Hipsters are a type of returning customer, we chose to make it a subclass
     # In case we want the hipster to have some different behavior later
-    def __init__(self):
-        super(Hipster, self).__init__()
+    def __init__(self, params):
+        super(Hipster, self).__init__(params)
         self.customer_type = 'hipster'
-        self.budget += 250  # Hipsters have an even larger budget
+        # Hipsters have an even larger budget
+        self.budget = params[self.customer_type]['budget']
+        self.inv_choice = params[self.customer_type]['inv_choice']
+
+    def make_choice(self, time, foods, drinks):
+
+        self.time = time
+
+        probs = list(foods.values())
+
+        if self.inv_choice:
+            probs = [(1 - x) for x in probs]
+            probs = [x/sum(probs) for x in probs]
+
+        self.food_choice = np.random.choice(list(foods.keys()), 1, p=probs)[0]
+
+        probs = list(drinks.values())
+
+        if self.inv_choice:
+            probs = [(1 - x) for x in probs]
+            probs = [x/sum(probs) for x in probs]
+
+        self.drink_choice = np.random.choice(list(drinks.keys()), 1, p=probs)[0]
 
 
 class TripAdvisorCustomer(Customer):
     # Trip advisor customer is a subclass of customer that adds a random tip to purchase
-    def __init__(self):
-        super(TripAdvisorCustomer, self).__init__()
+    def __init__(self, params):
+        super(TripAdvisorCustomer, self).__init__(params)
         self.customer_type = 'trip_advisor'
 
-    def make_payment(self):
+    def make_payment(self, menu):
         self.amount_spent = menu[self.food_choice] + \
             menu[self.drink_choice] + np.random.randint(1, 10)
         self.budget = self.budget - self.amount_spent
+
+
+class EmptyInterval(object):
+    def __init__(self, params):
+        self.customer_type = 'empty_interval'
+        self.customer_id = params[self.customer_type]
+        self.budget = params[self.customer_type]
+        self.name = params[self.customer_type]
+        self.food_choice = params[self.customer_type]
+        self.drink_choice = params[self.customer_type]
+        self.amount_spent = params[self.customer_type]
+        self.time = params[self.customer_type]
+
+    def make_choice(self, time, foods, drinks):
+        pass
+
+    def show_budget(self):
+        pass
+
+    def make_payment(self, menu):
+        pass
+
+    def tell_purchase(self):
+        print("Empty Interval")
 
 
 # Now test these classes for a few different actions the customers can do
