2e9c090d8557ca02531cf8a036e070d0feb4361e
Andy Boomer <aboomer07@gmail.com>
2020-11-11 15:48:45 -0800

    Inputting Conditional For Directory and Output Dir Definition

--- Code/SimEval.py
+++ Code/SimEval.py
@@ -15,11 +15,18 @@ import seaborn as sns
 import os
 import sys
 
+# Force the correct directory
+if os.getcwd().split("/")[-1] == "Code":
+    os.chdir("..")
+
+curr_dir = os.getcwd()
+output_dir = curr_dir + "/Output"
+
 
 def showcase_sim(sim, params, n_examples, to_text=False):
     # TODO: change to single string
     if to_text:
-        sys.stdout = open(os.path.abspath('..') + '/Output/testtext.txt', 'w')
+        sys.stdout = open(output_dir + '/testtext.txt', 'w')
     print('This showcases the results and functionality of simulated customer in the  following simulation:')
     print('Simulated period: ' + min(sim['time']).strftime("%d/%m/%Y, %H:%M") + ' to ' + max(sim['time']).strftime(
         "%d/%m/%Y, %H:%M") + ', n = ' + str(len(sim)))
@@ -37,8 +44,10 @@ def showcase_sim(sim, params, n_examples, to_text=False):
         customer.tell_purchase()
     print('\n')
     # returning customers know their history
-    print('A random selection of ' + str(n_examples) + ' exemplary histories of returning customers:')
-    returning_customers = list(sim[sim['customer_type'] == 'returning']['customer'])
+    print('A random selection of ' + str(n_examples) +
+          ' exemplary histories of returning customers:')
+    returning_customers = list(
+        sim[sim['customer_type'] == 'returning']['customer'])
     selected_customers = random.choices(returning_customers, k=n_examples)
     for customer in selected_customers:
         customer.tell_purchase_history()
@@ -49,7 +58,8 @@ def showcase_sim(sim, params, n_examples, to_text=False):
 
 def store_sim(sim, ind):
     df = sim.drop(['customer'], axis=1)
-    df.to_csv(os.path.abspath('..') + '/Output/simulated_data_' + ind + '.csv', sep=';', index=False)
+    df.to_csv(output_dir + '/simulated_data_' +
+              ind + '.csv', sep=';', index=False)
 
 
 def plot_sim(sim, ind):
@@ -73,7 +83,7 @@ def plot_sim(sim, ind):
     sns.countplot(x='drinks', data=sim, ax=axes[1])
     axes[1].set_title("Count of Drinks Purchases in 5-Year Simulation")
     fig.tight_layout()
-    plt.savefig(os.path.abspath('..') + '/Output/countplot_' + ind + '.png')
+    plt.savefig(output_dir + '/countplot_' + ind + '.png')
     plt.close()
 
     # Get the total amount spent over the five years per unique customer
@@ -85,7 +95,8 @@ def plot_sim(sim, ind):
 
     # Get the percentage of the max for the index and for the revenue
     cum_rev['index'] = cum_rev['index'] / cum_rev['index'].tail(1).values[0]
-    cum_rev['payments'] = cum_rev['payments'] / cum_rev['payments'].tail(1).values[0]
+    cum_rev['payments'] = cum_rev['payments'] / \
+        cum_rev['payments'].tail(1).values[0]
 
     # Initialize the figure
     fig, ax = plt.subplots(nrows=1, ncols=1)
@@ -98,7 +109,7 @@ def plot_sim(sim, ind):
     ax.set_title("Inequality Distribution of Total Spending by Customer")
     ax.set_xlabel("Percentile of Customer Index")  # Set x-axis title
     ax.set_ylabel("Percentile of Total Revenue")  # Set y-axis title
-    plt.savefig(os.path.abspath('..') + '/Output/cum_rev_' + ind + '.png')
+    plt.savefig(output_dir + '/cum_rev_' + ind + '.png')
     plt.close()
 
     # show development of returning customers pool
@@ -108,7 +119,7 @@ def plot_sim(sim, ind):
     ax.set_title("Size of returning customers pool")
     ax.set_xlabel("Time")  # Set x-axis title
     ax.set_ylabel("Amount of returning customers")  # Set y-axis title
-    plt.savefig(os.path.abspath('..') + '/Output/returns_size_' + ind + '.png')
+    plt.savefig(output_dir + '/returns_size_' + ind + '.png')
     plt.close()
 
     # revenue per day
@@ -119,17 +130,19 @@ def plot_sim(sim, ind):
     ax.set_title("Daily revenue")
     ax.set_xlabel("Time")  # Set x-axis title
     ax.set_ylabel("Total daily revenue in $")  # Set y-axis title
-    plt.savefig(os.path.abspath('..') + '/Output/daily_rev_' + ind + '.png')
+    plt.savefig(output_dir + '/daily_rev_' + ind + '.png')
     plt.close()
 
     # customer composition per day
     fig, ax = plt.subplots(nrows=1, ncols=1)
-    cust_struct = sim.groupby(['date', 'customer_type']).size().reset_index(name='counts')
-    cust_struct['total'] = cust_struct['counts'].groupby(cust_struct['date']).transform('sum')
+    cust_struct = sim.groupby(['date', 'customer_type']
+                              ).size().reset_index(name='counts')
+    cust_struct['total'] = cust_struct['counts'].groupby(
+        cust_struct['date']).transform('sum')
     cust_struct['perc'] = cust_struct['counts'] / cust_struct['total']
     sns.lineplot(x='date', y='perc', hue='customer_type', data=cust_struct)
     ax.set_title("Monthly Customer Composition")
     ax.set_xlabel("Time")  # Set x-axis title
     ax.set_ylabel("")  # Set y-axis title
-    plt.savefig(os.path.abspath('..') + '/Output/cust_comp_' + ind + '.png')
+    plt.savefig(output_dir + '/cust_comp_' + ind + '.png')
     plt.close()
