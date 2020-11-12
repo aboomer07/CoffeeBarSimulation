f3207b29acfc1cad56a63f74fc36f50bde4bfc61
Andy Boomer <aboomer07@gmail.com>
2020-11-11 20:22:38 -0800

    Adjusting Dirs

--- Code/Simulation.py
+++ Code/Simulation.py
@@ -1,9 +1,10 @@
-from Code.SimFunc import *
-from Code.SimEval import *
+# Force the correct directory
 from Code.SimParams import *
+from Code.SimEval import *
+from Code.SimFunc import *
 
 # set up simulation data frame
-sim_df = df[['time', 'year', 'hour', 'minute']][0:30000]
+sim_df = df[['time', 'year', 'hour', 'minute']]
 
 a = datetime.datetime.now()
 sim1_df = run_simulation(sim_df, Sim1)
@@ -25,18 +26,17 @@ a = datetime.datetime.now()
 sim5_df = run_simulation(sim_df, Sim5)
 print((datetime.datetime.now() - a).seconds)
 
-showcase_sim(sim1_df, Sim1, 2)
+# showcase_sim(sim1_df, Sim1, 2)
 plot_sim(sim1_df, 'sim_1')
 
-showcase_sim(sim2_df, Sim2, 2)
+# showcase_sim(sim2_df, Sim2, 2)
 plot_sim(sim2_df, 'sim_2')
 
-showcase_sim(sim3_df, Sim3, 2)
+# showcase_sim(sim3_df, Sim3, 2)
 plot_sim(sim3_df, 'sim_3')
 
-showcase_sim(sim4_df, Sim4, 2)
+# showcase_sim(sim4_df, Sim4, 2)
 plot_sim(sim4_df, 'sim_4')
 
-showcase_sim(sim5_df, Sim5, 2)
+# showcase_sim(sim5_df, Sim5, 2)
 plot_sim(sim5_df, 'sim_5')
-
